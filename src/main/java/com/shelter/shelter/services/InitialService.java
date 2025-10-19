51f20174a324b0ff5cfdbc82842324b7e3ff6324
Omar Cehajic <omar.cehajic@outlook.com>
2025-10-19 23:41:37 +0200

    Initial commit

---
+++ src/main/java/com/shelter/shelter/services/InitialService.java
@@ -0,0 +1,10 @@
+package com.shelter.shelter.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InitialService {
+    public String sayHi() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/shelter/shelter/services/InitialServiceTest.java
@@ -0,0 +1,19 @@
+package com.shelter.shelter.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class InitialServiceTest {
+    @InjectMocks
+    InitialService initialService;
+
+    @Test
+    void sayHi() {
+        Assertions.assertEquals("Hello World", initialService.sayHi());
+    }
+}
