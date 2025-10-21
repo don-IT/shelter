b9436cbd23ca6989ff350a5fefa994019596af83
Omar Cehajic <omar.cehajic@outlook.com>
2025-10-21 19:55:31 +0200

    add Employee entity

---
+++ src/main/java/com/shelter/shelter/entities/Employee.java
@@ -0,0 +1,37 @@
+package com.shelter.shelter.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
