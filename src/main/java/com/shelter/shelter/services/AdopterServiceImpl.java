fa9e5d4ee0e68252f561930d45679ed673eb1984
Omar Cehajic <omar.cehajic@outlook.com>
2025-11-24 17:32:09 +0100

    add basic AnimalServiceImpl implementation without unit testing

--- src/main/java/com/shelter/shelter/services/AdopterServiceImpl.java
+++ src/main/java/com/shelter/shelter/services/AdopterServiceImpl.java
@@ -5,39 +5,29 @@ import com.shelter.shelter.entities.Animal;
 import com.shelter.shelter.repositories.AdopterRepository;
 import java.util.List;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AdopterServiceImpl implements AdopterService {
   private final AdopterRepository adopterRepository;
 
-  public AdopterServiceImpl(AdopterRepository adopterRepository) {
-    this.adopterRepository = adopterRepository;
-  }
-
-  /**
-   */
   @Override
   public Adopter createAdopter(Adopter adopter) {
     return adopterRepository.save(adopter);
   }
 
-  /**
-   */
   @Override
   public Optional<Adopter> getAdopter(Long id) {
     return Optional.of(adopterRepository.findById(id)).orElse(null);
   }
 
-  /**
-   */
   @Override
   public List<Adopter> getAllAdopters() {
     return adopterRepository.findAll();
   }
 
-  /**
-   */
   @Override
   public List<Animal> getAdoptedAnimals(Long adopterId) {
     return adopterRepository.findAnimalsByAdopter(adopterId);
--- src/main/java/com/shelter/shelter/services/AnimalServiceImpl.java
+++ src/main/java/com/shelter/shelter/services/AnimalServiceImpl.java
@@ -2,70 +2,68 @@ package com.shelter.shelter.services;
 
 import com.shelter.shelter.entities.Animal;
 import com.shelter.shelter.entities.Status;
+import com.shelter.shelter.repositories.AnimalRepository;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class AnimalServiceImpl implements AnimalService {
-  /**
-   * @param animal
-   * @return
-   */
+  private final AnimalRepository animalRepository;
+  private final AdopterService adopterService;
+
   @Override
   public Animal createAnimal(Animal animal) {
-    return null;
+    return animalRepository.save(animal);
   }
 
-  /**
-   * @param id
-   * @param updated
-   * @return
-   */
   @Override
   public Animal updateAnimal(Long id, Animal updated) {
-    return null;
+    var optionalAnimal = animalRepository.findById(id);
+    if (optionalAnimal.isEmpty()) {
+      return null;
+    }
+    var animal = optionalAnimal.get();
+    animal.setName(updated.getName());
+    animal.setAge(updated.getAge());
+    animal.setStatus(updated.getStatus());
+    animal.setSpecies(updated.getSpecies());
+    animal.setAdopters(updated.getAdopters());
+    animal.setMedicalChecks(updated.getMedicalChecks());
+
+    return animal;
   }
 
-  /**
-   * @param id
-   */
   @Override
-  public void deleteAnimal(Long id) {}
+  public void deleteAnimal(Long id) {
+    animalRepository.deleteById(id);
+  }
 
-  /**
-   * @param id
-   * @return
-   */
   @Override
   public Animal getAnimal(Long id) {
-    return null;
+    return animalRepository.findById(id).orElse(null);
   }
 
-  /**
-   * @return
-   */
   @Override
   public List<Animal> getAllAnimals() {
-    return List.of();
+    return animalRepository.findAll();
   }
 
-  /**
-   * @param animalId
-   * @param adopterId
-   * @return
-   */
   @Override
+  @Transactional
   public Animal addAdopter(Long animalId, Long adopterId) {
-    return null;
+    animalRepository
+        .findById(animalId)
+        .ifPresent(
+            a -> adopterService.getAdopter(adopterId).ifPresent(ad -> a.getAdopters().add(ad)));
+    return animalRepository.findById(animalId).orElse(null);
   }
 
-  /**
-   * @param animalId
-   * @param status
-   * @return
-   */
   @Override
   public Animal updateStatus(Long animalId, Status status) {
-    return null;
+    animalRepository.findById(animalId).ifPresent(a -> a.setStatus(status));
+    return animalRepository.findById(animalId).orElse(null);
   }
 }
