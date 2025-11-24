fd79fca575e06d2131f8056a96b3f7d45f4a798d
Omar Cehajic <omar.cehajic@outlook.com>
2025-11-24 20:10:54 +0100

    apply spotless changes

--- src/main/java/com/shelter/shelter/entities/Employee.java
+++ src/main/java/com/shelter/shelter/entities/Employee.java
@@ -1,10 +1,9 @@
 package com.shelter.shelter.entities;
 
 import jakarta.persistence.*;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Entity
 @Setter
